a45fe4d5778487824029a1fd191e13b0a4ee869c
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-03-23 12:01:51 +0530

    Add function clearAll javascript.js

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ const clearBtn = document.querySelector(".clear")
 const equalsBtn = document.querySelector(".equals")
 const percentBtn = document.querySelector(".percentage")
 const toggleNegBtn = document.querySelector(".toggle-neg")
+
+let operandOne = 0
+let operandTwo = undefined
+let operator = undefined
+
+function clearAll() {
+    display.textContent = 0
+    operandOne = undefined
+    operandTwo = undefined
+    operator = undefined
+    bothOperatorsPresent = false
+}
