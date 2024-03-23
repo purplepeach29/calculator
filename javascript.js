a697261015c676fa7978a2bb7c6719f1d8e33800
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-03-23 12:00:39 +0530

    Add queryselector javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const display = document.querySelector(".display")
+const allNumbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const clearBtn = document.querySelector(".clear")
+const equalsBtn = document.querySelector(".equals")
+const percentBtn = document.querySelector(".percentage")
+const toggleNegBtn = document.querySelector(".toggle-neg")
